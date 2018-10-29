b301f7327941ec837a0a31a6667dcb77982e93d4
Cullen Self <cullen.self@utexas.edu>
2018-10-29 14:34:30 -0600

    Plot labels, add histogram to experiment comparision

--- parse.py
+++ parse.py
@@ -1,6 +1,10 @@
 import pandas
 from datetime import datetime
 
+# Histogram Bins
+BINS = list(range(-30,51,5)) # set up bins for histogram
+BINS[6] = 0.01 # adjust slightly so that temperature at exactly 0 show below rather than above (i.e. [-5,0.01) rather than [-5,0))
+
 def readFile(filename):
     df = pandas.read_csv(filename)
     df['timestamp'] = [datetime.utcfromtimestamp(time/float(1000)) for time in df['timestamp']]
@@ -11,19 +15,17 @@ def readFile(filename):
     return df
 
 def plotTime(df, sat):
-    plots = df.plot(x='timestamp', y=['batt.temp.bus1', 'batt.temp.bus2', 'batt.temp.bus3', 'batt.temp.bus4'], kind='line', subplots=True, figsize=(8,8), style='.', title='Battery Pack Temperature vs Time, Satellite %s' % sat, legend=True)
-    for plot in plots:
-        plot.set_xlabel('Date')
-        plot.set_ylabel('Temperature (C)')
-        plot.axhline(y=0)
+    axs = df.plot(x='timestamp', y=['batt.temp.bus1', 'batt.temp.bus2', 'batt.temp.bus3', 'batt.temp.bus4'], kind='line', subplots=True, figsize=(8,8), style='.', title='Battery Pack Temperature vs Time, Satellite %s' % sat, legend=True)
+    for ax in axs:
+        ax.set_xlabel('Date')
+        ax.set_ylabel('Temperature (C)')
+        ax.axhline(y=0, color='k')
     # depending on what I'm trying to show, I might set ylim=[-10,270] (or [-10,50] and ignore the outliers) to emphasize the relative temperature differences
 
 def plotDist(df, sat):
-    bins = list(range(-30,51,5)) # set up bins for histogram
-    bins[6] = 0.01 # adjust slightly so that temperature at exactly 0 show below rather than above (i.e. [-5,0.01) rather than [-5,0))
-    plots = df.plot(y=['batt.temp.bus1', 'batt.temp.bus2', 'batt.temp.bus3', 'batt.temp.bus4'], kind='hist', xlim=(-30,50), bins=bins, subplots=True, sharex=False, figsize=(8,8), title='Battery Pack Temperature Distribution, Satellite %s' % sat, legend=True)
-    for plot in plots:
-        plot.set_xlabel('Temperature (C)')
+    axs = df.plot(y=['batt.temp.bus1', 'batt.temp.bus2', 'batt.temp.bus3', 'batt.temp.bus4'], kind='hist', xlim=(-30,50), bins=BINS, subplots=True, sharex=False, figsize=(8,8), title='Battery Pack Temperature Distribution, Satellite %s' % sat, legend=True)
+    for ax in axs:
+        ax.set_xlabel('Temperature (C)')
     #TODO: better ylabels
 
 def plotReduce(df, label):
@@ -32,7 +34,15 @@ def plotReduce(df, label):
         for i in range(2, 5+1): 
             if row[i]:
                 temps.append((row.timestamp, row[i]))
-    plot = pandas.DataFrame(temps, columns=['timestamp', 'batt.temp']).plot(x='timestamp', y='batt.temp', kind='line', style='.', legend=False, title='Battery Pack Temperature vs Time (%s)' % label)
-    plot.set_ylabel('Temperature (C)')
-    plot.set_xlabel('Date')
+    df = pandas.DataFrame(temps, columns=['timestamp', 'batt.temp'])
+    ax = df.plot(x='timestamp', y='batt.temp', kind='line', style='.', legend=False, title='Battery Pack Temperature vs Time (%s)' % label)
+    ax.set_ylabel('Temperature (C)')
+    ax.set_xlabel('Date')
+    ax.axhline(y=0, color='k')
+
+    ax = df.plot(y='batt.temp', kind='hist', xlim=(-30,50), bins=BINS, density=True)
+    ax.set_ylabel('Frequency (%)')
+    ax.set_xlabel('Temperature (C)')
+
+    
     #TODO: format better
