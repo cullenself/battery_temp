993c8e85ac8be308c43cc721ffc50b2932a9ab38
Cullen Self <cullen.self@utexas.edu>
2018-10-30 20:00:26 -0600

    Documentation

--- parse.py
+++ parse.py
@@ -9,34 +9,39 @@ BINS[6] = 0.01 # adjust slightly so that temperature at exactly 0 show below rat
 INNER_BOUND = 15 # max acceptable difference between bus temps for a single epoch
 OUTER_BOUND = 5  # max acceptable change for a single bus between epochs
 
-def readFile(filename, ib=INNER_BOUND, ob=OUTER_BOUND):
+# Create Pandas DataFrame from CSV data
+def readFile(filename):
     df = pandas.read_csv(filename)
     df['timestamp'] = [datetime.utcfromtimestamp(time/float(1000)) for time in df['timestamp']]
-    return filterOutliers(df, ib, ob)
+    return df
 
-def filterOutliers(df, ib, ob):
+# Filter outliers from data (jumps in temp between readings, bus doesn't agree with other temps)
+def filterOutliers(df, IB=INNER_BOUND, OB=OUTER_BOUND):
     prev_temp = [None] * 4
     for i, row in df.iterrows():
         if (row['sc.bootcount'] > 0): 
             prev_temp = [None] * 4 # reset temperatures
             continue # no temp readings so skip row
         else:
+            # Check for self-consistency
             curr_temp = [row['batt.temp.bus1'], row['batt.temp.bus2'], row['batt.temp.bus3'], row['batt.temp.bus4']]
             curr_temp_median = median(curr_temp)
             for v in [max(curr_temp), min(curr_temp)]:
-                if (abs(v - curr_temp_median) > ib):
+                if (abs(v - curr_temp_median) > IB):
                     bus_num = curr_temp.index(v) + 1 
                     df.at[i, 'batt.temp.bus%d' % bus_num] = None
                     curr_temp[bus_num-1] = None
 
+            # Check for jumps between readings
             for bus_num in [1, 2, 3, 4]:
                 if ((prev_temp[bus_num-1]) and (curr_temp[bus_num-1])): # Can Compare
-                    if (abs(prev_temp[bus_num-1] - curr_temp[bus_num-1]) > ob):
+                    if (abs(prev_temp[bus_num-1] - curr_temp[bus_num-1]) > OB):
                         df.at[i, 'batt.temp.bus%d' % bus_num] = None # mark data as invalid
                         curr_temp[bus_num-1] = None
             prev_temp = curr_temp
     return df
 
+# Create a time series plot for each bus on a single satellite
 def plotTime(df, sat):
     axs = df.plot(x='timestamp', y=['batt.temp.bus1', 'batt.temp.bus2', 'batt.temp.bus3', 'batt.temp.bus4'], kind='line', subplots=True, figsize=(12,12), style='.', title='Battery Pack Temperature vs Time, Satellite %s' % sat, legend=True)
     for ax in axs:
@@ -44,11 +49,12 @@ def plotTime(df, sat):
         ax.set_ylabel('Temperature (C)')
         ax.axhline(y=0, color='k')
 
+# Create a histogram for all readings from a single satellite
 def plotDist(df, sat):
     ax = df.plot(y=['batt.temp.bus1', 'batt.temp.bus2', 'batt.temp.bus3', 'batt.temp.bus4'], kind='hist', xlim=(-30,50), bins=BINS, stacked=True, figsize=(8,8), title='Battery Pack Temperature Distribution, Satellite %s' % sat, legend=True)
     ax.set_xlabel('Temperature (C)')
-    #TODO: better ylabels
 
+# Consolidate DataFrame, dropping bus information, then plot time series and histogram
 def plotReduce(df, label):
     col = 'batt.temp.bus%d'
     temps = []
@@ -56,19 +62,22 @@ def plotReduce(df, label):
         for bus_num in [1, 2, 3, 4]: 
             temps.append({'timestamp': row['timestamp'], 'sat%s' % i[0]: row[col % bus_num]})
     df = pandas.DataFrame(temps)
-    df = df.dropna(thresh=2)
-    sats = list(df.columns)
+    df = df.dropna(thresh=2) # drop if both rows are NaN
+    sats = list(df.columns) # e.g. ['timestamp', 'satA', 'satB', ('satC', maybe) ]
     sats.remove('timestamp')
+
+    # Time series
     ax = df.plot(x='timestamp', y=sats, kind='line', style='.', alpha=0.5, title='Battery Pack Temperature vs Time (%s)' % label, legend=True)
     ax.set_ylabel('Temperature (C)')
     ax.set_xlabel('Date')
     ax.axhline(y=0, color='k')
-
+    # Histogram
     ax = df.plot(y=sats, kind='hist', xlim=(-30,50), bins=BINS, density=True, title='Battery Pack Temperature Distribution (%s)' % label, legend=True)
     ax.set_ylabel('Frequency (%)')
     ax.set_xlabel('Temperature (C)')
     return df
 
+# Count transitions (either heater on/off events, or dropping below/above 0C)
 def countCycles(df, low_thresh, high_thresh):
     col = 'batt.temp.bus%d'
     heater = [False] * 4 # True => Heater On, False => Heater Off
@@ -76,20 +85,20 @@ def countCycles(df, low_thresh, high_thresh):
     tot_time_on = timedelta()
     time_on = [None] * 4
     for i, row in df.iterrows():
-        if row['sc.bootcount'] > 0:
+        if row['sc.bootcount'] > 0: # Reboot event resets flags, and saves current times
             for bus in [1, 2, 3, 4]:
                 if heater[bus-1]:
                     heater[bus-1] = False
                     tot_time_on += row['timestamp'] - time_on[bus-1]
             continue
         for bus in [1, 2, 3, 4]:
-            if heater[bus-1]:
-                if row[col % bus] > high_thresh:
-                    heater[bus-1] = False
-                    tot_time_on += row['timestamp'] - time_on[bus-1]
-            if not heater[bus-1]:
-                if row[col % bus] < low_thresh:
-                    heater[bus-1] = True
-                    time_on[bus-1] = row['timestamp']
-                    count += 1
+            if heater[bus-1]: # if active
+                if row[col % bus] > high_thresh: # and exit active range
+                    heater[bus-1] = False # set unactive
+                    tot_time_on += row['timestamp'] - time_on[bus-1] # store active time
+            if not heater[bus-1]: # if not active
+                if row[col % bus] < low_thresh: # and enter active range
+                    heater[bus-1] = True # set active
+                    time_on[bus-1] = row['timestamp'] # store turn on time
+                    count += 1 # bump counter
     return count, tot_time_on
